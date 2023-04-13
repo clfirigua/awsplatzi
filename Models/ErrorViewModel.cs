2d9d3871549a1f41cf7c67ff6fd1642a77dbf343
Carlos Andres Firigua Contreras <71801587+clfirigua@users.noreply.github.com>
2023-04-12 20:43:35 -0500

    primer commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace platziwebSite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
